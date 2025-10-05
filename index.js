80367d6abf7ef3c66d9d3d44bdce1fd1c0680eb4
brianwaithaka <waithakabrian8@gmail.com>
2025-10-05 13:54:29 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", () => {
+  const pEl = document.querySelector("#text");
+  pEl.textContent = "This is really cool!";
+});
+// const pEl = document.querySelector("#text");
+// console.log(pEl);
